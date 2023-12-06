fca1846766c262fa75cd9bd6466a1924deab4e2c
Kamil Kośmider <78200901+shepard153@users.noreply.github.com>
2023-12-06 14:48:32 +0100

    Delete backend directory

--- backend/app/models/comment.rb
+++
@@ -1,5 +0,0 @@
-class Comment < ApplicationRecord
-    # belongs_to :user
-    belongs_to :post
-    self.per_page = 10
-end
--- backend/app/models/post.rb
+++
@@ -1,5 +0,0 @@
-class Post < ApplicationRecord
-    # belongs_to :user
-    has_many :comments
-    self.per_page = 10
-end
